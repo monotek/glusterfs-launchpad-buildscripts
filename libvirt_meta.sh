847325f0ad8348e15d734eec7f4ebc8bc32ca80f
André Bauer <abauer@magix.net>
2016-11-26 16:31:15 +0100

    added libvirt build scripts

---
+++ libvirt_meta.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# build libvirt with glusterfs support
+#
+
+if [ ! -f config ];then
+    echo "config missing! create config from config.dist example before running this script!"
+    exit 1
+fi
+
+#config
+OS_VERSION="$1"
+GLUSTER_VERSION="$2"
+PACKAGE="libvirt-bin"
+
+. "config"
+
+#script
+if [ -z ${OS_VERSION} ] || [ -z ${GLUSTER_VERSION} ]; then
+    echo -e "need os and gluster version! \nUsage: $0 trusty 3.6.2"
+    exit 1
+fi
+
+export DEBFULLNAME=${DEBFULLNAME}
+
+export DEBEMAIL=${DEBEMAIL}
+
+sudo sh -c "sed "s/#OS_VERSION#/${OS_VERSION}/g" < sources.list.d/ubuntu-src.dist > /etc/apt/sources.list.d/ubuntu-src.list"
+
+sudo apt-get update
+
+test -d ${PACKAGEDIR} && rm -r ${PACKAGEDIR}
+
+mkdir -p ${PACKAGEDIR}
+
+cd ${PACKAGEDIR}
+
+apt-get source ${PACKAGE}/${OS_VERSION}
+
+REAL_PATH="$(realpath .)"
+
+cd $(find ${REAL_PATH} -maxdepth 2 -mindepth 2 -type d -iname debian)
+
+debchange -l ${PACKAGE_IDENTIFIER} ${DEBCOMMENT} -D ${OS_VERSION}
+
+cp control control.org
+
+sed 's# netcat-openbsd,# netcat-openbsd,\n glusterfs-common,#g' < control.org > control
+
+rm control.org
+
+if [ "${LAUNCHPAD_UPLOAD}" == "yes" ]; then
+    debuild -S
+
+    dput ppa:${PPA_OWNER}/${PPA} $(find ${REAL_PATH} -name libvirt*gluster*_source.changes | sort | tail -n 1)
+else
+    debuild -us -uc -i -I
+fi
+
---
+++ libvirt_new.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# build libvirt with glusterfs support
+#
+
+if [ ! -f config ];then
+    echo "config missing! create config from config.dist example before running this script!"
+    exit 1
+fi
+
+#config
+OS_VERSION="$1"
+GLUSTER_VERSION="$2"
+PACKAGE="libvirt-bin"
+
+. "config"
+
+#script
+if [ -z ${OS_VERSION} ] || [ -z ${GLUSTER_VERSION} ]; then
+    echo -e "need os and gluster version! \nUsage: $0 trusty 3.5"
+    exit 1
+fi
+
+export DEBFULLNAME=${DEBFULLNAME}
+
+export DEBEMAIL=${DEBEMAIL}
+
+if [ "${3}" == "nodelete" ]; then
+    echo "no sync! uploading new version!"
+else
+    rsync -av --delete ${BUILD_DIR}/#source_downloads/libvirt-bin/ ${PACKAGEDIR}
+fi
+
+cd ${PACKAGEDIR}
+
+cd $(find ${PACKAGEDIR} -maxdepth 1 -mindepth 1 -type d -name "*${PACKAGE}*")/debian
+
+debchange -l ${PACKAGE_IDENTIFIER} ${DEBCOMMENT} -D ${OS_VERSION}
+
+cp control control.org
+
+sed 's# netcat-openbsd,# netcat-openbsd,\n glusterfs-common,#g' < control.org > control
+
+rm control.org
+
+if [ "${LAUNCHPAD_UPLOAD}" == "yes" ]; then
+    debuild -S
+
+    dput ppa:${PPA_OWNER}/${PPA} $(find ${PACKAGEDIR} -name ${PACKAGE}*gluster*_source.changes | sort | tail -n 1)
+else
+    debuild -us -uc -i -I
+fi
