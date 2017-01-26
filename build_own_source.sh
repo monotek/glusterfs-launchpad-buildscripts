24ad417ab67d3ac3f031348fbd3e460259dee186
André Bauer <abauer@magix.net>
2017-01-26 12:08:06 +0100

    changed sed commands

--- build_own_source.sh
+++ build_own_source.sh
@@ -56,7 +56,9 @@ elif [ "${PACKAGE}" == "qemu" ]; then
 elif [ "${PACKAGE}" == "samba" ]; then
     sed 's#bison,#bison,\n               glusterfs-common,\n               libacl1-dev,#' < control.org > control
 elif [ "${PACKAGE}" == "tgt" ]; then
-    sed 's#, bash-completion#, bash-completion, glusterfs-common, libacl1-dev#' < control.org > control
+    sed -i -e 's#, libsystemd-dev#, libsystemd-dev, glusterfs-common, libacl1-dev#' -e 's#Package: tgt-dbg#Package: tgt-glusterfs\nArchitecture: linux-any\nDepends: tgt (= ${binary:Version}), ${shlibs:Depends}, ${misc:Depends}\nDescription: Linux SCSI target user-space daemon and tools - GlusterFS support\n The Linux target framework (tgt) allows a Linux system to provide SCSI\n devices (targets) over networked SCSI transports.\n .\n tgt consists of a user-space daemon and user-space tools currently\n supporting the following transports:\n .\n  - iSCSI (SCSI over IP)\n  - iSER (iSCSI over RDMA, using Infiniband)\n .\n tgt also supports different storage types for use as backing stores for SCSI\n Logical Units:\n .\n  - Plain files and block devices\n  - Ceph/RADOS RBD volumes\n  - GlusterFS volumes\n .\n This package enables tgt to use GlusterFS volumes as backing store for SCSI\n Logical Units.\n\nPackage: tgt-dbg#' control
+    echo "debian/tgt/usr/lib/tgt/backing-store/bs_glfs.so usr/lib/tgt/backing-store" > tgt-glusterfs.install
+    sed -i 's#CEPH_RBD=1#CEPH_RBD=1 GLFS_BD=1#' rules
 fi
 
 rm control.org
